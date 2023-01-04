2098ce5515d8120e232fb05a4cf117ffaaffc806
CPJiNan <2759278070@qq.com>
2023-01-04 15:09:37 +0800

    [+] 新增事件查询系统

--- src/main/kotlin/me/cpjinan/playerban/commands/MainCommand.kt
+++ src/main/kotlin/me/cpjinan/playerban/commands/MainCommand.kt
@@ -1,9 +1,14 @@
 package me.cpjinan.playerban.commands
 
+import me.cpjinan.playerban.manager.ConfigManager
+import me.cpjinan.playerban.manager.ConfigManager.data
+import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.command.CommandBody
 import taboolib.common.platform.command.CommandHeader
 import taboolib.common.platform.command.mainCommand
+import taboolib.common.platform.command.subCommand
 import taboolib.expansion.createHelper
+import taboolib.module.chat.colored
 
 @CommandHeader(name = "playerban")
 object MainCommand {
@@ -11,6 +16,17 @@ object MainCommand {
     @CommandBody
     val main = mainCommand {
         createHelper()
+    }
+
+    @CommandBody
+    val reason = subCommand {
+        dynamic("id") {
+            execute<ProxyCommandSender> { sender, context, argument ->
+                val reason : String? = (data.getString("player."+context.get(1).substringAfter("#")+".reason"))?.colored()
+                if (reason != null) sender.sendMessage(("&c玩家 "+context.get(1).substringAfter("#")+" 的封禁原因为: &f"+ reason).colored())
+                else sender.sendMessage(("&c不存在编号为 "+context.get(1)+" 的封禁事件!").colored())
+            }
+        }
     }
 
     @CommandBody
